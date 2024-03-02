c3de6a04365c21688cfc91830a8f03453fab76ce
Dmytrotarn <112310528+Dmytrotarn@users.noreply.github.com>
2024-03-02 10:34:10 +0200

    add break; after chilie and Jamaica

--- js/task-4.js
+++ js/task-4.js
@@ -6,11 +6,13 @@ function getShippingCost(country) {
       break;
     case 'Chile':
       message = `Shipping to ${country} will cost 250 credits`;
+      break;
     case 'Australia':
       message = `Shipping to ${country} will cost 170 credits`;
       break;
     case 'Jamaica':
       message = `Shipping to ${country} will cost 120 credits`;
+      break;
     default:
       message = 'Sorry, there is no delivery to your country';
       break;
